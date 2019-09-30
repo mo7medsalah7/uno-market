a91742c8d90e593f01d286cbfc450bcd4baea6c9
unknown <msmido9@gmail.com>
2019-09-30 14:08:13 +0200

    Add new package

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,5 @@
-exports.getHomePage = (req, res, next) => {
-  res.render("index", {
+exports.getHomePage = async (req, res, next) => {
+  await res.render("index", {
     isLoggedIn: req.isAuthenticated()
   });
 };
